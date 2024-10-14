d33d733e8823e3380dad962ae7359ae8f41d1bfb
Saya__IB <indrabayu210903@gmail.com>
2024-10-15 01:59:57 +0700

    feat : webview implementation

---
+++ lib/app/modules/product_detail/views/product_detail_web_view.dart
@@ -0,0 +1,26 @@
+import 'package:get/get.dart';
+import 'package:flutter/material.dart';
+import 'package:liedle/app/data/model/brands.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+import 'package:liedle/app/modules/product_detail/controller/product_detail_controller.dart';
+
+class ProductDetailWebView extends GetView<ProductDetailController> {
+  final BrandElement brand; // Change this to BrandElement
+  const ProductDetailWebView({
+    super.key,
+    required this.brand,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("WebView"),
+      ),
+      body: WebViewWidget(
+        controller:
+            controller.webViewController(brand.website), // Use brand.website
+      ),
+    );
+  }
+}
